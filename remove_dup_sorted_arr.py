e2c1aa3b133234900e9d98b3071573864ef1b85c
Zuheb khan <zuhebk433@gmail.com>
2026-04-05 19:19:29 +0530

    solved

--- remove_dup_sorted_arr.py
+++ remove_dup_sorted_arr.py
@@ -7,6 +7,16 @@ from pydantic_core.core_schema import none_schema
 # If current value == next value → skip the next node
 
 class Node:
-    def __init__(self):
+    def __init__(self,data):
         self.data=data
-        self.next=
+        self.next=None
+
+def remove_dup(head):
+    cur=head
+
+    while cur and cur.next:
+        if cur.data==cur.next.data:
+            cur.next=cur.next.next
+        else:
+            cur=cur.next
+    return head()
